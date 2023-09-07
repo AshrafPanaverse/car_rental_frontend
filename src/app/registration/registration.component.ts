6ec3558c476e17811278c134db353f0b11d29195
Ashraf Hussain <125198812+AshrafPanaverse@users.noreply.github.com>
2023-09-07 21:52:33 +0500

    Registration or SignUP Module Added

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent {
+  user = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    address: '',
+    companyName: '',
+    vatNumber: '',
+    agree: false,
+  };
 
+  onSubmit() {
+    alert('Submited Successfully!')
+    // Here, you can add code to handle the form submission
+    // Typically, you would call your UserService to perform the signup operation
+    console.log('User submitted:', this.user);
+    // Add your signup logic here, e.g., calling a service method
+  }
 }
